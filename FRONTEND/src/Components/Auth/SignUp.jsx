70b62cfcd1e5534ff9ce7b4d7c8d65251cf028b4
uroojbutt <zbutturooj@gmail.com>
2025-08-05 21:34:56 +0500

    added fullname input

--- FRONTEND/src/Components/Auth/SignUp.jsx
+++ FRONTEND/src/Components/Auth/SignUp.jsx
@@ -10,6 +10,11 @@ export default function Signup() {
         <form>
           <input
             type="text"
+            placeholder="Full Name"
+            className="w-full p-3 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          <input
+            type="text"
             placeholder="Username"
             className="w-full p-3 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
           />
