8f20be0247786a68a63c155e2d5bfb804934a561
Hamza Riaz <hriaz9803@gmail.com>
2025-11-15 00:09:46 -0800

    Remove unnecessary database connection options for cleaner configuration

--- BACKEND/src/Utils/db.utils.js
+++ BACKEND/src/Utils/db.utils.js
@@ -1,13 +1,9 @@
 import mongoose from "mongoose";
 
-
 const dbOptions = {
     maxPoolSize: process.env.MAX_POOL_SIZE,
     minPoolSize: process.env.MIN_POOL_SIZE,
-    serverSelectionTimeoutMS: 5000,
     socketTimeoutMS: 60000,
-    retryWrites: true,
-    w: 'majority',
 }
 
 const connectDB = async () => {
